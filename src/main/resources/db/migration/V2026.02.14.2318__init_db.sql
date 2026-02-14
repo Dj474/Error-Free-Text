1091915e8fe67333531fc4286eeab3689650c973
Dj474 <toxap06@gmail.com>
2026-02-14 23:38:27 +0300

    fix: db init

--- src/main/resources/db/migration/V2026.02.14.2318__init_db.sql
+++ src/main/resources/db/migration/V2026.02.14.2318__init_db.sql
@@ -1,8 +1,7 @@
 CREATE TABLE IF NOT EXISTS users (
         id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
-        username VARCHAR(50) NOT NULL UNIQUE,
         login VARCHAR(100) NOT NULL UNIQUE,
-        password_hash VARCHAR(255) NOT NULL,
+        password VARCHAR(255) NOT NULL,
         created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
         updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
@@ -13,7 +12,7 @@ CREATE TABLE IF NOT EXISTS task (
        status VARCHAR(20) NOT NULL,
        source_text TEXT NOT NULL,
        corrected_text TEXT,
-       source_language VARCHAR(2) NOT NULL,
+       language VARCHAR(2) NOT NULL,
        error_message TEXT,
        created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
        updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
