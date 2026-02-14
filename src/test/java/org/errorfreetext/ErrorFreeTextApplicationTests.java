82f107366deb63d8e9c3590686a512921b704b6b
Dj474 <toxap06@gmail.com>
2026-02-14 22:55:23 +0300

    init project

---
+++ src/test/java/org/errorfreetext/ErrorFreeTextApplicationTests.java
@@ -0,0 +1,13 @@
+package org.errorfreetext;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ErrorFreeTextApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
